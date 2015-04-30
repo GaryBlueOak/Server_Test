a8d363bba131713ab50d404387b3e87d4716c4c7
Raymond Chow <raychow1944@yahoo.com>
2015-04-30 14:01:29 -0400

    Fixing

--- app/src/main/java/com/doggel/server_test/MainActivity.java
+++ app/src/main/java/com/doggel/server_test/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -11,6 +12,9 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getActionBar().hide();
         setContentView(R.layout.activity_main);
     }
 
