89a1760f2c644957120231c1c199d70fbddb4e3a
Patty <psmithdb9@gmail.com>
2015-05-04 11:21:12 -0400

    Added some new realistic JSON based server code. Needs to be modified to fit the login type interface.

--- app/src/main/java/com/doggel/server_test/MainActivity.java
+++ app/src/main/java/com/doggel/server_test/MainActivity.java
@@ -1,175 +1,64 @@
 package com.doggel.server_test;
 
-import android.content.Context;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.app.Activity;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+public class MainActivity extends Activity {
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
-import java.net.URLEncoder;
-
-
-public class MainActivity extends ActionBarActivity {
-    private EditText username,password;
-    private Button enter;
-    private TextView status;
-
+    JSONObject jobj = null;
+    ServerSide clientServerInterface = new ServerSide();
+    TextView textView;
+    String ab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
-        getActionBar().hide();
-
-        username = (EditText)findViewById(R.id.username);
-        password = (EditText)findViewById(R.id.password);
-        enter = (Button)findViewById(R.id.enter);
-        status = (TextView)findViewById(R.id.status);
-
-        enter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String name = username.getText().toString() + "";
-                String pass = username.getText().toString() + "";
-                //String stat = "LOGIN STATUS: "; //concat to this for status
-
-                onEnterLogin(name,pass);
-            }
-        });
-
         setContentView(R.layout.activity_main);
+        textView = (TextView) findViewById(R.id.status);
+        //start background processing
+        new RetreiveData().execute();
     }
-
-    private void onEnterLogin(String name, String pass) {
-        //signInBackground likely to change
-        //everything else is "functional" or at least in the right form.
-        new SignInBackground(this,status,0).execute(name,pass);
-
-    }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        //getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    class RetreiveData extends AsyncTask<String,String,String>{
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public class SignInBackground extends AsyncTask<String, Void, String>{
-
-        private Context context;
-        private int operationFlag; // 0 for get, 1 for post
-        private TextView tv;
-
-        public SignInBackground(Context context,TextView tv, int flag){
-            this.context = context;
-            operationFlag = flag;
-            this.tv = tv;
-        }
         @Override
-        protected String doInBackground(String... params) {
-            switch(operationFlag){
-                case 0:
-                    try{
-                        String username = (String)params[0];
-                        String password = (String)params[1];
-                        String link = ""; //link to mysql web serv
-                        // "http://myphpmysqlweb.hostei.com/login.php?username=" +username+"&password="+password
-                        URL url = new URL(link);
-                        HttpClient client = new DefaultHttpClient();
-                        HttpGet request = new HttpGet();
-                        request.setURI(new URI(link));
-                        HttpResponse response = client.execute(request);
-                        BufferedReader in = new BufferedReader(new InputStreamReader((response.getEntity().getContent())));
+        protected String doInBackground(String... arg0) {
+            // where we will put our mysql url
+            jobj = clientServerInterface.makeHttpRequest("http://www.yourwebsite.com/at/serverside.php");
 
-                        StringBuffer buff = new StringBuffer("");
-                        String line ="";
-                        while((line = in.readLine())!=null){
-                            buff.append(line);
-                            //break?
-                        }
-                        in.close();
-                        return buff.toString();
-                    }catch(Exception e){
-
-                        return new String("Exception: " + e.getMessage());
-                    }
-
-                case 1:
-                    try{
-                        String username = (String)params[0];
-                        String password = (String)params[1];
-                        String link="http://myphpmysqlweb.hostei.com/loginpost.php";
-                        String data  = URLEncoder.encode("username", "UTF-8")
-                                + "=" + URLEncoder.encode(username, "UTF-8");
-                        data += "&" + URLEncoder.encode("password", "UTF-8")
-                                + "=" + URLEncoder.encode(password, "UTF-8");
-                        URL url = new URL(link);
-                        URLConnection conn = url.openConnection();
-                        conn.setDoOutput(true);
-                        OutputStreamWriter wr = new OutputStreamWriter
-                                (conn.getOutputStream());
-                        wr.write( data );
-                        wr.flush();
-                        BufferedReader reader = new BufferedReader
-                                (new InputStreamReader(conn.getInputStream()));
-                        StringBuilder sb = new StringBuilder();
-                        String line = null;
-                        // Read Server Response
-                        while((line = reader.readLine()) != null)
-                        {
-                            sb.append(line);
-                            break;
-                        }
-                        return sb.toString();
-
-                    }catch(Exception e){
-                        return new String("Exception: " + e.getMessage());
-                    }
-
-                default:
-                    return new String("Error: Flag Operation not Recognized");
+            try {
+                ab = jobj.getString("key");
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-
-
-
-
+            return ab;
         }
-        @Override
-         protected void onPostExecute(String result){
-            this.tv.setText("LOGIN STATUS: Successful");
+        protected void onPostExecute(String ab){
+
+            textView.setText(ab);
         }
-    }
+
 }
+
+}
+/* This is the PHP that should be added.
+<?php
+        $con = mysql_connect("localhost","username","password");
+        mysql_select_db("database_name",$con);
+        $result = mysql_query("SELECT * FROM pizza_table");
+        $arr = array();
+        echo json_encode($arr);
+        ?>
+*/
